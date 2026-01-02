9eecb5f7920a4477416741c948fe6a3627d8facf
SnehaJain1601 <jain.sneha1601@gmail.com>
2026-01-02 21:09:08 +0530

    add

---
+++ DAY1/max3.js
@@ -0,0 +1,8 @@
+//max of 3 numbers
+let a=45
+let b = 85
+let c =10
+if(a>b && a>c) console.log("First Number is greatest");
+if(b>a && b>c) console.log("Second Number is greatest");
+else console.log("Third Number is greatest");
+
