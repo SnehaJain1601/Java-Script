d27341759c77c602f99c1ddf28cf97fa4477dd58
SnehaJain1601 <jain.sneha1601@gmail.com>
2026-01-02 20:42:24 +0530

    add practise from chatgpt

---
+++ practise.js
@@ -0,0 +1,37 @@
+//1...................
+// console.log(a);
+// var a = 10; //OUTPUT undefined
+
+// console.log(b);//OUTPUT ReferenceError
+// let b = 20;
+
+//2...............
+// {
+//   var x = 5;
+//   let y = 10;
+// }
+// console.log(x);
+// console.log(y); //OUTPUT let have block scope so shows error y is not defined
+
+//3........................OUTPUT Hello
+// greet();
+
+// function greet() {
+//   console.log("Hello");
+// }
+
+//4........................OUTPUT TypeError: sayHi is not a function
+// sayHi();
+
+// var sayHi = function () {
+//   console.log("Hi");
+// };
+
+//5........................OUTPUT 12 3 51 1
+console.log(1 + "2");
+console.log("5" - 2);
+console.log("5" + 2 - 1);
+console.log(true + false);
+
+
+
