582cc34bd8951331a248ee8f74ecf022380d569f
SnehaJain1601 <jain.sneha1601@gmail.com>
2026-01-03 10:07:18 +0530

    Add Array.js

---
+++ JS/Array.js
@@ -0,0 +1,27 @@
+//ForEach and Map 
+// let fruits = new Array("apple" , "orange" , "banana");
+// fruits.forEach((curEleme ,index , arr)=>{
+//     console.log(`${curEleme} ${index}`);
+// })
+
+// fruits.map((curEleme ,index , arr)=>{
+//     console.log(`${curEleme} ${index}`);
+// })
+//.......................................................................
+
+//Difference between two forEach and Map
+// const myForEachArr = fruits.forEach((curEleme ,index , arr)=>{
+//     return`${curEleme} ${index}`;
+// })
+
+// const myMapArr = fruits.map((curEleme ,index , arr)=>{
+//     return`${curEleme} ${index}`;
+// })
+
+//.....................................................................
+// console.log(fruits.indexOf("apple"));
+// console.log(fruits.includes("apple"));
+//................................................
+let fruits = ["apple", "orange", "banana"];
+console.log(fruits[3]);
+console.log(fruits["apple"]);
