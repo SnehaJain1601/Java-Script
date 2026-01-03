4c52bad35b6fec21a10cfc1ff5736fc35d6ee60b
SnehaJain1601 <jain.sneha1601@gmail.com>
2026-01-03 10:11:45 +0530

    Add array

--- JS/Array.js
+++ JS/Array.js
@@ -24,4 +24,4 @@
 //................................................
 let fruits = ["apple", "orange", "banana"];
 console.log(fruits[3]);
-console.log(fruits["apple"]);
+console.log(fruits["apple"]);git 
