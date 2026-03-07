bf0751455fb419b461dcff0a315f01f460607ee1
SnehaJain1601 <jain.sneha1601@gmail.com>
2026-03-07 11:21:27 +0530

    JSON

--- objects/index.js
+++ objects/index.js
@@ -149,3 +149,26 @@ let person = {
 // const isEqual = obj1 == obj3 ? true : false;
 // console.log(isEqual);
 
+//* =====================================
+//* JSON (JavaScript Object Notation):
+//* ======================================
+
+//?JSON is a data interchange format derived from JavaScript objects. Objects can be easily converted to JSON and vice versa.
+
+// let student = {
+//   id: 1,
+//   sName: "Vinod",
+//   sAge: 29,
+//   isStudent: false,
+//   greet: function () {
+//     console.log(
+//       `hey my id is ${student.identity} & my name is ${student.sName}`
+//     );
+//   },
+// };
+
+// let jsonData = JSON.stringify(student);
+// console.log(jsonData);
+// let parsedObject = JSON.parse(jsonData);
+// console.log(parsedObject);
+
